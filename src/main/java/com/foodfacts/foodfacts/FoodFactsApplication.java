8be1da6d0e20c1ff956b2abfabd8d2e6aed03bad
MQRF123 <u20221e734@upc.edu.pe>
2024-11-04 01:52:31 +0000

    Backend: the project is created

---
+++ src/main/java/com/foodfacts/foodfacts/FoodFactsApplication.java
@@ -0,0 +1,13 @@
+package com.foodfacts.foodfacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodfacts/foodfacts/FoodFactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodfacts.foodfacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
