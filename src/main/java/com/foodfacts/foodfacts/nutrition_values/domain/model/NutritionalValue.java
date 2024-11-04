15aac3e9f06bd48cd7575960e132084fa9447bc4
Ernesto Casaverde <u20221b657@upc.edu.pe>
2024-11-04 16:45:10 -0500

    fix: product bounden context

--- src/main/java/com/foodfacts/foodfacts/nutrition_values/domain/model/NutritionalValue.java
+++ src/main/java/com/foodfacts/foodfacts/nutrition_values/domain/model/NutritionalValue.java
@@ -1,25 +1,40 @@
 package com.foodfacts.foodfacts.nutrition_values.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
+import com.foodfacts.foodfacts.products.domain.model.Product;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
 public class NutritionalValue {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String portionSize;
-    private Integer calories;
-    private Integer carbohydrates;
-    private Integer sugar;
-    private Integer sodium;
-    private Integer fat;
+    @Setter
+    @Getter
+    private double portionSize;
+    @Getter
+    @Setter
+    private int calories;
+    @Setter
+    @Getter
+    private double carbohydrates;
+    @Setter
+    @Getter
+    private double sugar;
+    @Getter
+    @Setter
+    private double sodium;
+    @Getter
+    @Setter
+    private double fat;
 
-    // Constructores, getters y setters generados por Lombok
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    // Getters and setters...
+
 }
--- src/main/java/com/foodfacts/foodfacts/products/domain/model/Product.java
+++ src/main/java/com/foodfacts/foodfacts/products/domain/model/Product.java
@@ -1,75 +1,29 @@
 package com.foodfacts.foodfacts.products.domain.model;
+
 import com.foodfacts.foodfacts.nutrition_values.domain.model.NutritionalValue;
 import jakarta.persistence.*;
-import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Set;
+
+@Setter
+@Getter
 @Entity
-@Table(name = "products")
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
     private String description;
-
-    @ManyToOne
-    @JoinColumn(name = "category_id", nullable = false)
-    private Category category;
-
-    @ManyToOne
-    @JoinColumn(name = "brand_id", nullable = false)
-    private Brand brand;
+    private String category;
+    private String brand;
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<NutritionalValue> nutritionalValues;
+    private Set<NutritionalValue> nutritionalValues;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
+    // Getters and setters...
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public Brand getBrand() {
-        return brand;
-    }
-
-    public void setBrand(Brand brand) {
-        this.brand = brand;
-    }
-
-    public List<NutritionalValue> getNutritionalValues() {
-        return nutritionalValues;
-    }
-
-    public void setNutritionalValues(List<NutritionalValue> nutritionalValues) {
-        this.nutritionalValues = nutritionalValues;
-    }
 }
