d9d7e5b8dca5e8f8e899028f05ae52351b181a87
Ernesto Casaverde <u20221b657@upc.edu.pe>
2024-11-20 12:56:12 -0500

    fix: correo

--- src/main/java/com/foodfacts/foodfacts/users/interfaces/rest/UsersController.java
+++ src/main/java/com/foodfacts/foodfacts/users/interfaces/rest/UsersController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:4200")
 @Tag(name = "User", description = "Available User Endpoints")
 public class UsersController {
 
