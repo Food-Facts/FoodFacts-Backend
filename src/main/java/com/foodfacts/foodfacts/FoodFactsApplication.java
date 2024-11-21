fb5faa12ebfa37ddb73ed4e638429e595ed40625
Ernesto Casaverde <u20221b657@upc.edu.pe>
2024-11-20 21:03:12 -0500

    .
    ..

--- src/main/java/com/foodfacts/foodfacts/FoodFactsApplication.java
+++ src/main/java/com/foodfacts/foodfacts/FoodFactsApplication.java
@@ -2,11 +2,7 @@ package com.foodfacts.foodfacts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -16,17 +12,4 @@ public class FoodFactsApplication {
 		SpringApplication.run(FoodFactsApplication.class, args);
 	}
 
-	@Configuration
-	public static class MyConfiguration {
-		@Bean
-		public WebMvcConfigurer corsConfigurer() {
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
-	}
 }
