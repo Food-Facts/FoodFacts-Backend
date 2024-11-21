ce9ca2bc8749df2a0f3217de69505c352c3948bf
Ernesto Casaverde <u20221b657@upc.edu.pe>
2024-11-20 19:18:26 -0500

    .
    .

--- src/main/java/com/foodfacts/foodfacts/SwaggerRedirectController.java
+++ src/main/java/com/foodfacts/foodfacts/SwaggerRedirectController.java
@@ -1,6 +1,5 @@
 package com.foodfacts.foodfacts;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
--- src/main/java/com/foodfacts/foodfacts/products/infrastructure/presistence/jpa/repositories/ProductRepository.java
+++ src/main/java/com/foodfacts/foodfacts/products/infrastructure/presistence/jpa/repositories/ProductRepository.java
@@ -1,7 +1,5 @@
 package com.foodfacts.foodfacts.products.infrastructure.presistence.jpa.repositories;
 
-
-
 import com.foodfacts.foodfacts.products.domain.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
